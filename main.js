48b3269b7143c107142383a0137186826c36defd
aesop <aesop@velozo.com>
2024-05-30 16:00:49 +0000

    changed design, still not working

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 class covecoin
 {
-    coins = 0;
-    
+    constructor()
+    {
+        this.coins = 1;
+        this.mineTotal = 0;
+        this.button = document.getElementById("mine");
+        this.button.addEventListener("click", mine());
+        const displayElement = document.getElementById("balance");
+        this.updateDisplay();
+    }
+    updateDisplay() 
+    {
+        displayElement.innerText = `CoveCoins: ${this.mineTotal}`;
+    }
+    mine()
+    {
+        this.mineTotal += this.coins;
+        console.log("+${this.coins} coins");
+        updateDisplay();
+    }    
 }
